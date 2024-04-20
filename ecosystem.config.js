afd5cc92b0dbba03c869503ff2fb508ab060312e
sunyang <2480901422@qq.com>
2024-04-20 17:47:58 +0800

    save

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,11 @@
-export const apps = [
-  {
-    name: 'blog-nuxt',
-    port: '3000',
-    exec_mode: 'cluster',
-    instances: 'max',
-    script: './.output/server/index.mjs',
-  },
-]
+module.exports = {
+  apps: [
+    {
+      name: 'blog-nuxt',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
