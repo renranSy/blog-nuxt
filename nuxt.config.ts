8c5e0bc0c9edb85a6956da3ec4d8a6a9492f9a57
sunyang <2480901422@qq.com>
2024-05-30 01:16:39 +0800

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,12 +60,5 @@ export default defineNuxtConfig({
         'xml'
       ]
     }
-  },
-  appConfig: {
-    umami: {
-      host: 'http://81.68.86.65:3000/',
-      id: '1f1a69a1-746a-4665-9cbc-97441c0eea6b',
-      autoTrack: true
-    }
   }
 })
