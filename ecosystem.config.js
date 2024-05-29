82368701c1434e489d58a57f110fa0dbb7f615e7
sunyang <2480901422@qq.com>
2024-05-29 23:43:10 +0800

    save

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'blog-nuxt',
-      port: '3000',
+      port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+  devServer: {
+    port: 3001
+  },
   modules: [
     'nuxt-primevue',
     '@unocss/nuxt',
