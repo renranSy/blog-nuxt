9f6838d0d8ff9d4afa5507b8692c6a9a74f5b639
sunyang <2480901422@qq.com>
2024-05-30 01:31:13 +0800

    save

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,8 @@
 export default defineAppConfig({
   umami: {
     host: 'https://blog.renranz.cn',
-    id: '1f1a69a1-746a-4665-9cbc-97441c0eea6b'
+    id: '1f1a69a1-746a-4665-9cbc-97441c0eea6b',
+    version: 2,
+    customEndpoint: '/api/send'
   }
 })
